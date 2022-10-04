84381ee74ba02142e06a9c18ac364e554fe589d5
Joe Dineen <jdineen@gmail.com>
2022-10-04 22:57:46 +0100

    changed settings location

--- jdineen_django/wsgi.py
+++ jdineen_django/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jdineen_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jdineen_django.settings.production')
 
 application = get_wsgi_application()
